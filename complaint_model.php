38f0fe21d3a759861d4a3729f8f6f41463488a8a
zaaid1989 <zaaid@rozesolutions.com>
2016-10-14 15:49:16 +0500

    Products Model Removed

--- complaint_model.php
+++ complaint_model.php
@@ -792,6 +792,26 @@ class Complaint_model extends CI_Model
             	return $dbresResult;
 			}
 	  }
+	  
+	/////////////////************** FUNCTIONS OF PRODUCTS MODEL *****************/////////////////
+	public function get_instruments_model()
+	{
+			$dbres = $this->db->query("SELECT * FROM tbl_instruments  order by `pk_instrument_id` DESC");
+            $dbresResult=$dbres->result_array();
+            return $dbresResult;
+	}
+	public function get_parts_model()
+	{
+			$dbres = $this->db->query("SELECT * FROM tbl_parts  order by `pk_part_id` DESC");
+            $dbresResult=$dbres->result_array();
+            return $dbresResult;
+	}
+	public function get_complaint_data($complaint_id)
+	{
+		$dbres = $this->db->query("SELECT * FROM tbl_complaints where pk_complaint_id = '".$complaint_id."'");
+		$dbresResult=$dbres->result_array();
+		return $dbresResult;
+	}
 	
 	
   }
